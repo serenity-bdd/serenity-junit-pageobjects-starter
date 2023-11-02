6dcd43381fc69d6f76a1efb562570ba8e935331b
John Ferguson Smart <john.smart@wakaleo.com>
2023-11-02 20:44:19 +0000

    Added sample solution for the first tutorial

---
+++ src/test/java/starter/duckduckgo/SearchResultSidebar.java
@@ -0,0 +1,9 @@
+package starter.duckduckgo;
+
+import net.serenitybdd.core.pages.PageComponent;
+
+public class SearchResultSidebar extends PageComponent {
+    public String heading() {
+        return $("[data-testid=about] h2").getText();
+    }
+}
